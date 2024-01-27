495f31c18223b57c616a3b6114149fd4c7e08876
nm1008 <mallari08nikko@yahoo.com>
2024-01-27 12:31:57 +0800

    implementing zustand, state, api fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
 import "./App.css";
 import useCounter from "./store";
 
 function App() {
-  const { counter, increaseCounter, removeCount } = useCounter();
+  const { counter, increaseCounter, removeCount, fetchData, data, nextPage } = useCounter();
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
 
   return (
     <>
@@ -10,6 +15,10 @@ function App() {
       <h4>{counter}</h4>
       <button onClick={increaseCounter}>Press to add</button>
       <button onClick={removeCount}>Press to remove</button>
+      {console.log(nextPage)}
+      {data.map((pokemon) => {
+        return <div key={pokemon.name}>{pokemon.name}</div>
+      })}
     </>
   );
 }
--- src/store.js
+++ src/store.js
@@ -4,6 +4,21 @@ const useCounter = create((set) => ({
   counter: 0,
   increaseCounter: () => set((state) => ({ counter: state.counter + 1 })),
   removeCount: () => set({ counter: 0 }),
+  data: [],
+  nextPage: null,
+  loading: false,
+  error: null,
+  fetchData: async () => {
+    try {
+      set({ loading: true, error: null });
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const result = await response.json();
+      set({ data: result.results, nextPage: result.next, loading: false });
+    } catch (error) {
+      set({ loading: false, error: "Error on fetching data" });
+      console.log("Error on fetching data");
+    }
+  },
 }));
 
 export default useCounter;
